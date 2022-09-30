575f9ffbed6d0e774498b0fac931c349977f7244
apa210 <apa_210@hotmail.com>
2022-09-30 03:14:38 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Se definen los arrays de palos y cartas
+  let palos = ["corazon", "diamante", "picas", "trebol"];
+  let cartas = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+  let card = document.querySelector(".card"); //Se obtiene la card
+  let cardValor = document.querySelector("h1"); //Se obtiene el numero de la card
+
+  //Se asigna un valor random al valor de la carta
+  cardValor.innerHTML = cartas[getRandom(0, cartas.length)];
+
+  //Se asigna un palo random
+  card.classList.add(palos[getRandom(0, palos.length)]);
+
+  //Boton
+  document.querySelector(".btn").textContent = "Generar";
+  document.querySelector(".btn").addEventListener("click", function() {
+    for (let i = 0; i < palos.length; i++) {
+      if (card.classList.contains(palos[i])) {
+        card.classList.remove(palos[i]);
+
+        card.classList.add(palos[getRandom(0, palos.length)]);
+      }
+    }
+
+    cardValor.innerHTML = cartas[getRandom(0, cartas.length)];
+  });
 };
+
+//Devuelve un número aleatorio entre el mínimo y el máximo
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
